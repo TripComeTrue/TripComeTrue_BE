fbbc81ef0fcd1ca12b2f4dba6031735b93df13cf
jo0oy <rkfkrh07@naver.com>
2023-12-30 14:32:20 +0900

    Setting: Spring Boot 프로젝트 초기 셋업

---
+++ src/main/java/com/haejwo/tripcometrue/TripcometrueApplication.java
@@ -0,0 +1,13 @@
+package com.haejwo.tripcometrue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripcometrueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripcometrueApplication.class, args);
+	}
+
+}
