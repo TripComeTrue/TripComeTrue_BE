3d0131d02fc8a71a9625c0b1d5d398b22bed745c
liyusang1 <liyusang1@naver.com>
2024-01-02 00:58:37 +0900

    [feat] ErrorCode 클래스 추가
    ErrorCode 클래스 추가

---
+++ src/main/java/com/haejwo/tripcometrue/global/exception/ErrorCode.java
@@ -0,0 +1,36 @@
+package com.haejwo.tripcometrue.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * @author liyusang1
+ * @implNote 에러 메시지 코드들을 한번에 관리
+ */
+
+@Getter
+public enum ErrorCode {
+
+    // EMAIL
+    EMAIL_SENDING_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 전송에 실패했습니다."),
+    EMAIL_TEMPLATE_LOAD_FAILURE(HttpStatus.INTERNAL_SERVER_ERROR, "이메일 템플릿 로드에 실패했습니다."),
+    EMAIL_VERIFY_FAILURE(HttpStatus.BAD_REQUEST, "인증번호가 일치하지 않습니다."),
+
+    // USER
+    USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 회원입니다."),
+    USER_ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 가입된 회원입니다."),
+
+    // AUTH
+    INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 틀렸습니다."),
+
+    // 5xx
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 에러");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    ErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
