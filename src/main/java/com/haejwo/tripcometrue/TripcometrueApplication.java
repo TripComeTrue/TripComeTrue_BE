77c1ad71e27c491443fde8d3d23dbe9c69be99f4
liyusang1 <liyusang1@naver.com>
2024-01-02 01:11:45 +0900

    [feat] yml 설정 추가 및 EnableJpaAuditing적용
    yml 설정 추가 및 EnableJpaAuditing적용

--- src/main/java/com/haejwo/tripcometrue/TripcometrueApplication.java
+++ src/main/java/com/haejwo/tripcometrue/TripcometrueApplication.java
@@ -2,8 +2,10 @@ package com.haejwo.tripcometrue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TripcometrueApplication {
 
 	public static void main(String[] args) {
