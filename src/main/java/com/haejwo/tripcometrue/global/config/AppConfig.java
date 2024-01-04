e2a4a4e12d540f4bf18c6e9d7e9fc16c986da655
liyusang1 <liyusang1@naver.com>
2024-01-05 03:23:02 +0900

    [fix] bCryptPasswordEncoder authconfig에서 설정 하도록 변경
    bCryptPasswordEncoder authconfig에서 설정 하도록 변경

--- src/main/java/com/haejwo/tripcometrue/global/config/AppConfig.java
+++ src/main/java/com/haejwo/tripcometrue/global/config/AppConfig.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * @author liyusang1
@@ -14,10 +13,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
