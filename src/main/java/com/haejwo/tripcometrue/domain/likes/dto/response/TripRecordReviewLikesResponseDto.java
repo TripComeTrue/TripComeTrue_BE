de62ed764ff2d836c30ef0b4e442a53fa7790a6a
INKWON <dlsrnjs066@gmail.com>
2024-01-09 03:06:47 +0900

    [feat] Response Dto 경로 수정

--- src/main/java/com/haejwo/tripcometrue/domain/likes/dto/response/TripRecordReviewLikesResponseDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/likes/dto/response/TripRecordReviewLikesResponseDto.java
@@ -1,4 +1,4 @@
-package com.haejwo.tripcometrue.domain.likes.dto;
+package com.haejwo.tripcometrue.domain.likes.dto.response;
 
 import com.haejwo.tripcometrue.domain.likes.entity.TripRecordReviewLikes;
 import lombok.Builder;
