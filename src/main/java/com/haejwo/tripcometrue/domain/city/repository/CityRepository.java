a9452bb16e6bf6af0332da41a5e2c48b3dd647b3
jo0oy <rkfkrh07@naver.com>
2024-01-08 15:53:05 +0900

    [feat] CityRepository 추가
    related to #27

---
+++ src/main/java/com/haejwo/tripcometrue/domain/city/repository/CityRepository.java
@@ -0,0 +1,7 @@
+package com.haejwo.tripcometrue.domain.city.repository;
+
+import com.haejwo.tripcometrue.domain.city.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
+}
