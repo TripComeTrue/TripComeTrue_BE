0187ef0b753436f38bdf3787f0999a7227504d4a
INKWON <dlsrnjs066@gmail.com>
2024-01-09 03:03:06 +0900

    [feat] Likes Response DTO 추가

---
+++ src/main/java/com/haejwo/tripcometrue/domain/likes/dto/PlaceReviewLikesResponseDto.java
@@ -0,0 +1,20 @@
+package com.haejwo.tripcometrue.domain.likes.dto;
+
+import com.haejwo.tripcometrue.domain.likes.entity.PlaceReviewLikes;
+import lombok.Builder;
+
+
+@Builder
+public record PlaceReviewLikesResponseDto(
+    Long likeId,
+    Long memberId,
+    Long placeReviewId
+) {
+
+  public static PlaceReviewLikesResponseDto fromEntity(PlaceReviewLikes placeReviewLikes) {
+    return new PlaceReviewLikesResponseDto(
+        placeReviewLikes.getId(),
+        placeReviewLikes.getMember().getId(),
+        placeReviewLikes.getPlaceReview().getId());
+  }
+}
