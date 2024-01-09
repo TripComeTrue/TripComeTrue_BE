44940f0e729cbb5a449bfcf1e3431d031d8d423e
junmo95 <junmop950@gmail.com>
2024-01-09 13:06:25 +0900

    [feat] 여행후기 스케쥴(trip_record_schedule)관련 응답 객체 구현 - 여행후기 스케쥴 상세 응답객체(여행후기 상세 페이지에 사용예정) - 여행후기 스케쥴 이미지 응답객체 - 여행후기 스케쥴 비디오 응답객체

---
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/dto/response/triprecord_schedule/TripRecordScheduleDetailResponseDto.java
@@ -0,0 +1,62 @@
+package com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord_schedule;
+
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.schedule_media.TripRecordScheduleImageResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.schedule_media.TripRecordScheduleVideoResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordSchedule;
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordScheduleImage;
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordScheduleVideo;
+import java.util.List;
+import lombok.Builder;
+
+public record TripRecordScheduleDetailResponseDto(
+    Long id,
+    Integer dayNumber,
+    Integer ordering,
+    String content,
+    Long memberId,
+    Long tripRecordId,
+    List<TripRecordScheduleImageResponseDto> images,
+    List<TripRecordScheduleVideoResponseDto> videos
+) {
+
+    @Builder
+    public TripRecordScheduleDetailResponseDto(Long id, Integer dayNumber, Integer ordering,
+        String content, Long memberId, Long tripRecordId,
+        List<TripRecordScheduleImageResponseDto> images,
+        List<TripRecordScheduleVideoResponseDto> videos) {
+        this.id = id;
+        this.dayNumber = dayNumber;
+        this.ordering = ordering;
+        this.content = content;
+        this.memberId = memberId;
+        this.tripRecordId = tripRecordId;
+        this.images = images;
+        this.videos = videos;
+    }
+
+    public static TripRecordScheduleDetailResponseDto fromEntity(TripRecordSchedule entity) {
+
+        List<TripRecordScheduleImage> images = entity.getTripRecordScheduleImages();
+        List<TripRecordScheduleImageResponseDto> imageDtos = images.stream()
+                                    .map(TripRecordScheduleImageResponseDto::fromEntity)
+                                    .toList();
+
+        List<TripRecordScheduleVideo> videos = entity.getTripRecordScheduleVideos();
+        List<TripRecordScheduleVideoResponseDto> videoDtos = videos.stream()
+                                    .map(TripRecordScheduleVideoResponseDto::fromEntity)
+                                    .toList();
+
+        return TripRecordScheduleDetailResponseDto.builder()
+            .id(entity.getId())
+            .dayNumber(entity.getDayNumber())
+            .ordering(entity.getOrdering())
+            .content(entity.getContent())
+            .memberId(entity.getMember() != null ? entity.getMember().getId() : null)
+            .tripRecordId(entity.getTripRecord() != null ? entity.getTripRecord().getId() : null)
+            .images(imageDtos)
+            .videos(videoDtos)
+            .build();
+    }
+
+
+}
