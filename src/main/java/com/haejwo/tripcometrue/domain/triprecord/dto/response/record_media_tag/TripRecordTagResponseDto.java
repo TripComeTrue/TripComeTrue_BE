2acba535271f04540f009f05d56a84491659fc7f
junmo95 <junmop950@gmail.com>
2024-01-09 13:05:29 +0900

    [feat] 여행후기(trip_record)관련 응답 객체 구현 - 여행후기 상세 응답객체(여행후기 상세 페이지에 사용예정) - 여행후기 이미지 응답객체 - 여행후기 채그 응답객체

---
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/dto/response/record_media_tag/TripRecordTagResponseDto.java
@@ -0,0 +1,28 @@
+package com.haejwo.tripcometrue.domain.triprecord.dto.response.record_media_tag;
+
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordTag;
+import com.haejwo.tripcometrue.domain.triprecord.entity.type.HashTagType;
+import lombok.Builder;
+
+public record TripRecordTagResponseDto(
+    Long id,
+    HashTagType hashTagType,
+    Long tripRecordId
+) {
+
+    @Builder
+    public TripRecordTagResponseDto(Long id, HashTagType hashTagType, Long tripRecordId) {
+        this.id = id;
+        this.hashTagType = hashTagType;
+        this.tripRecordId = tripRecordId;
+    }
+
+    public static TripRecordTagResponseDto fromEntity(TripRecordTag entity) {
+        return TripRecordTagResponseDto.builder()
+            .id(entity.getId())
+            .hashTagType(entity.getHashTagType())
+            .tripRecordId(entity.getTripRecord().getId())
+            .build();
+    }
+
+}
