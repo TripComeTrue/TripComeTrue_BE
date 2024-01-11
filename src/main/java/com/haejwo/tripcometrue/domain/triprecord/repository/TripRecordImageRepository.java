edcdb8e746095325dd21af62b75b952654c36cbf
liyusang1 <liyusang1@naver.com>
2024-01-12 02:18:40 +0900

    [feat] TripRecord관련 Repository 추가
    TripRecord관련 Repository 추가

---
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/repository/TripRecordImageRepository.java
@@ -0,0 +1,8 @@
+package com.haejwo.tripcometrue.domain.triprecord.repository;
+
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TripRecordImageRepository extends JpaRepository<TripRecordImage, Long> {
+
+}
