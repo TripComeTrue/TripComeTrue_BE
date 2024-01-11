7dc4500ea877d8f34f527e8b94e9522c99ee8e57
junmo95 <junmop950@gmail.com>
2024-01-11 14:49:12 +0900

    [feat] 여행후기 리스트 조회 기능 응답 DTO 추가

---
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/dto/response/TripRecordListResponseDto.java
@@ -0,0 +1,38 @@
+package com.haejwo.tripcometrue.domain.triprecord.dto.response;
+
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.member.TripRecordMemberResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecord;
+import lombok.Builder;
+
+public record TripRecordListResponseDto(
+    Long id,
+    String title,
+    String countries,
+    Integer totalDays,
+    Integer commentNumber,
+    Integer storeNumber,
+    TripRecordMemberResponseDto member
+) {
+
+    @Builder
+    public TripRecordListResponseDto(Long id, String title, String countries, Integer totalDays,
+        Integer commentNumber, Integer storeNumber, TripRecordMemberResponseDto member) {
+        this.id = id;
+        this.title = title;
+        this.countries = countries;
+        this.totalDays = totalDays;
+        this.commentNumber = commentNumber;
+        this.storeNumber = storeNumber;
+        this.member = member;
+    }
+
+    public static TripRecordListResponseDto fromEntity(TripRecord entity) {
+        return TripRecordListResponseDto.builder()
+            .id(entity.getId())
+            .title(entity.getTitle())
+            .countries(entity.getCountries())
+            .totalDays(entity.getTotalDays())
+            .build();
+    }
+
+}
