83a6e27ed7ff547e75ec89b457019697e0ff4ba7
liyusang1 <liyusang1@naver.com>
2024-01-12 04:38:01 +0900

    [fix] conflict 방지를 위해 controller service 새로 추가
    conflict 방지를 위해 controller service 새로 추가

---
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/controller/TripRecordAddController.java
@@ -0,0 +1,67 @@
+package com.haejwo.tripcometrue.domain.triprecord.controller;
+
+import com.haejwo.tripcometrue.domain.triprecord.dto.request.CreateSchedulePlaceRequestDto;
+import com.haejwo.tripcometrue.domain.triprecord.dto.request.TripRecordRequestDto;
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord_schedule.SearchScheduleTripResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.service.TripRecordAddService;
+import com.haejwo.tripcometrue.global.enums.Country;
+import com.haejwo.tripcometrue.global.springsecurity.PrincipalDetails;
+import com.haejwo.tripcometrue.global.util.ResponseDTO;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/trip-record")
+@RequiredArgsConstructor
+public class TripRecordAddController {
+
+    private final TripRecordAddService tripRecordAddService;
+
+    @PostMapping
+    public ResponseEntity<ResponseDTO<Void>> tripRecordAdd(
+        @AuthenticationPrincipal PrincipalDetails principalDetails,
+        @RequestBody TripRecordRequestDto requestDto
+    ) {
+
+        tripRecordAddService.addTripRecord(principalDetails, requestDto);
+        ResponseDTO<Void> responseBody = ResponseDTO.ok();
+
+        return ResponseEntity
+            .status(responseBody.getCode())
+            .body(responseBody);
+    }
+
+    @GetMapping("/search-schedule-places")
+    public ResponseEntity<ResponseDTO<List<SearchScheduleTripResponseDto>>> searchSchedulePlace(
+        @RequestParam Country country,
+        @RequestParam String city
+    ) {
+        ResponseDTO<List<SearchScheduleTripResponseDto>> responseBody
+            = ResponseDTO.okWithData(tripRecordAddService.searchSchedulePlace(country, city));
+
+        return ResponseEntity
+            .status(responseBody.getCode())
+            .body(responseBody);
+    }
+
+    @PostMapping("/schedule-place")
+    public ResponseEntity<ResponseDTO<Long>> createSchedulePlace(
+        @RequestBody CreateSchedulePlaceRequestDto createSchedulePlaceRequestDto
+    ) {
+        ResponseDTO<Long> responseBody
+            = ResponseDTO.okWithData(
+            tripRecordAddService.createSchedulePlace(createSchedulePlaceRequestDto));
+
+        return ResponseEntity
+            .status(responseBody.getCode())
+            .body(responseBody);
+    }
+}
