cc0b655dedeab9a6abdcde9b279837be3d89b466
jo0oy <rkfkrh07@naver.com>
2024-01-12 05:06:41 +0900

    [feat] CityResponseDto Builder 추가, @JsonInclude NON_NULL 설정 추가
    related to #55

--- src/main/java/com/haejwo/tripcometrue/domain/city/dto/response/CityResponseDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/city/dto/response/CityResponseDto.java
@@ -1,7 +1,10 @@
 package com.haejwo.tripcometrue.domain.city.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.haejwo.tripcometrue.domain.city.entity.City;
+import lombok.Builder;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record CityResponseDto(
     Long id,
     String name,
@@ -14,18 +17,23 @@ public record CityResponseDto(
     String weatherDescription,
     String country
 ) {
+
+    @Builder
+    public CityResponseDto {
+    }
+
     public static CityResponseDto fromEntity(City entity) {
-        return new CityResponseDto(
-            entity.getId(),
-            entity.getName(),
-            entity.getLanguage(),
-            entity.getTimeDifference(),
-            entity.getVoltage(),
-            entity.getVisa(),
-            entity.getCurrency(),
-            entity.getWeatherRecommendation(),
-            entity.getWeatherDescription(),
-            entity.getCountry().getDescription()
-        );
+        return CityResponseDto.builder()
+            .id(entity.getId())
+            .name(entity.getName())
+            .language(entity.getLanguage())
+            .timeDifference(entity.getTimeDifference())
+            .voltage(entity.getVoltage())
+            .visa(entity.getVisa())
+            .currency(entity.getCurrency().name())
+            .weatherRecommendation(entity.getWeatherRecommendation())
+            .weatherDescription(entity.getWeatherDescription())
+            .country(entity.getCountry().getDescription())
+            .build();
     }
 }
