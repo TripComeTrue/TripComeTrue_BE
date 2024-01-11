7dd4e14584ba0009775a763bf261bc926b32ed4a
junmo95 <junmop950@gmail.com>
2024-01-11 14:47:07 +0900

    [feat] 여행후기 리스트 조회 기능 Controller 추가 - 추후에 쿼리항목이 늘어날 것이라 예상 @ModelAttribute를 통해서 객체로 쿼리문 요청을 받아냄

--- src/main/java/com/haejwo/tripcometrue/domain/triprecord/controller/TripRecordController.java
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/controller/TripRecordController.java
@@ -1,21 +1,28 @@
 package com.haejwo.tripcometrue.domain.triprecord.controller;
 
+import com.haejwo.tripcometrue.domain.triprecord.dto.TripRecordListRequestAttribute;
 import com.haejwo.tripcometrue.domain.triprecord.dto.request.TripRecordRequestDto;
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.TripRecordListResponseDto;
 import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord.TripRecordDetailResponseDto;
 import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord.TripRecordResponseDto;
 import com.haejwo.tripcometrue.domain.triprecord.service.TripRecordService;
 import com.haejwo.tripcometrue.global.springsecurity.PrincipalDetails;
 import com.haejwo.tripcometrue.global.util.ResponseDTO;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -53,6 +60,23 @@ public class TripRecordController {
             .body(responseBody);
     }
 
+    @GetMapping
+    public ResponseEntity<ResponseDTO<List<TripRecordListResponseDto>>> tripRecordList(
+        Pageable pageable,
+        @ModelAttribute TripRecordListRequestAttribute request
+    ) {
+
+        List<TripRecordListResponseDto> responseDtos
+            = tripRecordService.findTripRecordList(pageable, request);
+
+        ResponseDTO<List<TripRecordListResponseDto>> responseBody = ResponseDTO.okWithData(responseDtos);
+
+        return  ResponseEntity
+                    .status(responseBody.getCode())
+                    .body(responseBody);
+
+    }
+
     @PutMapping("/{tripRecordId}")
     public ResponseEntity<ResponseDTO<TripRecordResponseDto>> tripRecordModify(
         @AuthenticationPrincipal PrincipalDetails principalDetails,
@@ -82,9 +106,5 @@ public class TripRecordController {
             .status(responseBody.getCode())
             .body(responseBody);
     }
-
-
-
-
 
 }
