a9a4e151e1c8bb6d4e93070aa8448d94912c10f3
liyusang1 <liyusang1@naver.com>
2024-01-12 02:32:07 +0900

    [feat] CityRepository findByNameAndCountry 추가
    CityRepository findByNameAndCountry 추가

--- src/main/java/com/haejwo/tripcometrue/domain/city/repository/CityRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/city/repository/CityRepository.java
@@ -1,7 +1,11 @@
 package com.haejwo.tripcometrue.domain.city.repository;
 
 import com.haejwo.tripcometrue.domain.city.entity.City;
+import com.haejwo.tripcometrue.global.enums.Country;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
+
+    Optional<City> findByNameAndCountry(String name, Country country);
 }
