fecf9b00d92d45b6e20e14d6963bc1e8c05f621f
liyusang1 <liyusang1@naver.com>
2024-01-12 02:31:21 +0900

    [feat] place entity 위도경도 값 추가 및 findByCityId 추가
    place entity 위도경도 값 추가 및 findByCityId 추가

--- src/main/java/com/haejwo/tripcometrue/domain/place/repositroy/PlaceRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/place/repositroy/PlaceRepository.java
@@ -1,11 +1,12 @@
 package com.haejwo.tripcometrue.domain.place.repositroy;
 
 import com.haejwo.tripcometrue.domain.place.entity.Place;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaceRepository extends
     JpaRepository<Place, Long>,
     PlaceCustomRepository
 {
-
+    List<Place> findByCityId(Long cityId);
 }
