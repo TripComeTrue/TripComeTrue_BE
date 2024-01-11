11095120fb27276440979ae524f2e1b89fcad88a
liyusang1 <liyusang1@naver.com>
2024-01-12 03:43:09 +0900

    [feat] TripPlan 생성 관련 controller, service, repository 추가
    TripPlan 생성 관련 controller, service, repository 추가

---
+++ src/main/java/com/haejwo/tripcometrue/domain/tripplan/controller/TripPlanController.java
@@ -0,0 +1,35 @@
+package com.haejwo.tripcometrue.domain.tripplan.controller;
+
+import com.haejwo.tripcometrue.domain.tripplan.dto.request.TripPlanRequestDto;
+import com.haejwo.tripcometrue.domain.tripplan.sevice.TripPlanService;
+import com.haejwo.tripcometrue.global.springsecurity.PrincipalDetails;
+import com.haejwo.tripcometrue.global.util.ResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/trip-plan")
+@RequiredArgsConstructor
+public class TripPlanController {
+
+    private final TripPlanService tripPlanService;
+
+    @PostMapping
+    public ResponseEntity<ResponseDTO<Void>> tripPlanAdd(
+        @AuthenticationPrincipal PrincipalDetails principalDetails,
+        @RequestBody TripPlanRequestDto requestDto
+    ) {
+
+        tripPlanService.addTripPlan(principalDetails, requestDto);
+        ResponseDTO<Void> responseBody = ResponseDTO.ok();
+
+        return ResponseEntity
+            .status(responseBody.getCode())
+            .body(responseBody);
+    }
+}
