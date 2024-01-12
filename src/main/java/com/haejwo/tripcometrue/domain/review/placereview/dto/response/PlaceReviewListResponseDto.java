0e19d73d3a2c19cabd52d2189fd59c8b1a93747d
INKWON <dlsrnjs066@gmail.com>
2024-01-13 06:30:09 +0900

    [feat] MY여행지 후기 리뷰 조회 기능 구현

---
+++ src/main/java/com/haejwo/tripcometrue/domain/review/placereview/dto/response/PlaceReviewListResponseDto.java
@@ -0,0 +1,26 @@
+package com.haejwo.tripcometrue.domain.review.placereview.dto.response;
+
+import com.haejwo.tripcometrue.domain.review.placereview.entity.PlaceReview;
+import java.time.LocalDateTime;
+
+public record PlaceReviewListResponseDto(
+    Long id,
+    String content,
+    String imageUrl,
+    Integer likeCount,
+    Long memberId,
+    Long placeId,
+    LocalDateTime createdAt
+) {
+  public static PlaceReviewListResponseDto fromEntity(PlaceReview placeReview) {
+    return new PlaceReviewListResponseDto(
+        placeReview.getId(),
+        placeReview.getContent(),
+        placeReview.getImageUrl(),
+        placeReview.getLikeCount(),
+        placeReview.getMember().getId(),
+        placeReview.getPlace().getId(),
+        placeReview.getCreatedAt()
+    );
+  }
+}
--- src/main/java/com/haejwo/tripcometrue/domain/review/placereview/repository/PlaceReviewRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/review/placereview/repository/PlaceReviewRepository.java
@@ -3,6 +3,8 @@ package com.haejwo.tripcometrue.domain.review.placereview.repository;
 import com.haejwo.tripcometrue.domain.member.entity.Member;
 import com.haejwo.tripcometrue.domain.place.entity.Place;
 import com.haejwo.tripcometrue.domain.review.placereview.entity.PlaceReview;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,5 @@ public interface PlaceReviewRepository extends JpaRepository<PlaceReview, Long>,
 
     boolean existsByMemberAndPlace(Member member, Place place);
 
+  List<PlaceReview> findByMemberId(Long memberId, Pageable pageable);
 }
