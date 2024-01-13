ba84c4e4db584026fe881b1af5e05de030c25539
liyusang1 <liyusang1@naver.com>
2024-01-13 22:32:46 +0900

    [feat] TripRecord관련 repository에 deleteAllBy 추가
    TripRecord관련 repository에 deleteAllBy 추가

--- src/main/java/com/haejwo/tripcometrue/domain/triprecord/repository/triprecord_schedule/TripRecordScheduleRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/repository/triprecord_schedule/TripRecordScheduleRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TripRecordScheduleRepository extends JpaRepository<TripRecordSchedule, Long> {
 
+
+    void deleteAllByTripRecordId(Long recordId);
 }
