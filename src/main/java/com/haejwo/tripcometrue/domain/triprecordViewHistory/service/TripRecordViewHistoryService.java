ad0f26a877d1a68c5fa539c9407fe9899c5df644
INKWON <dlsrnjs066@gmail.com>
2024-01-14 23:44:11 +0900

    [fix] develop 브랜치 패키지 경로 변경사항 반영

--- src/main/java/com/haejwo/tripcometrue/domain/triprecordViewHistory/service/TripRecordViewHistoryService.java
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecordViewHistory/service/TripRecordViewHistoryService.java
@@ -1,9 +1,10 @@
 package com.haejwo.tripcometrue.domain.triprecordViewHistory.service;
+
 import com.haejwo.tripcometrue.domain.member.entity.Member;
 import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecord;
 import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordViewHistory;
 import com.haejwo.tripcometrue.domain.triprecord.exception.TripRecordNotFoundException;
-import com.haejwo.tripcometrue.domain.triprecord.repository.TripRecordRepository;
+import com.haejwo.tripcometrue.domain.triprecord.repository.triprecord.TripRecordRepository;
 import com.haejwo.tripcometrue.domain.triprecordViewHistory.dto.response.TripRecordViewHistoryResponseDto;
 import com.haejwo.tripcometrue.domain.triprecordViewHistory.repository.TripRecordViewHistoryRepository;
 import com.haejwo.tripcometrue.global.springsecurity.PrincipalDetails;
