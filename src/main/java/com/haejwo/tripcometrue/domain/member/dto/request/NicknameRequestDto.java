92670cf16233992527da3ce4b349f6a2182a0e9b
INKWON <dlsrnjs066@gmail.com>
2024-01-16 17:37:00 +0900

    [refac] 비밀번호 검증 관련 로직 변경 및 DTO간소화

--- src/main/java/com/haejwo/tripcometrue/domain/member/dto/request/NicknameRequestDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/member/dto/request/NicknameRequestDto.java
@@ -3,8 +3,6 @@ package com.haejwo.tripcometrue.domain.member.dto.request;
 import com.haejwo.tripcometrue.domain.member.dto.response.NicknameResponseDto;
 import com.haejwo.tripcometrue.domain.member.entity.Member;
 
-public record NicknameRequestDto(String nickname) {
-  public static NicknameResponseDto fromEntity(Member member) {
-    return new NicknameResponseDto(member.getMemberBase().getNickname());
-  }
-}
+public record NicknameRequestDto(
+    String nickname
+) {}
--- src/main/java/com/haejwo/tripcometrue/domain/member/dto/request/ProfileImageRequestDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/member/dto/request/ProfileImageRequestDto.java
@@ -2,6 +2,4 @@ package com.haejwo.tripcometrue.domain.member.dto.request;
 
 public record ProfileImageRequestDto(
     String profile_image
-) {
-
-}
+) {}
