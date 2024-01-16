ee100004f45ae7cb6f32674e18bd577ae04d7491
jo0oy <rkfkrh07@naver.com>
2024-01-16 16:33:38 +0900

    [feat] 도시 상세 피드 핫플/여행지 리스트 조회 쿼리 구현
    related to: #66

--- src/main/java/com/haejwo/tripcometrue/domain/place/repositroy/PlaceCustomRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/place/repositroy/PlaceCustomRepository.java
@@ -4,6 +4,7 @@ import com.haejwo.tripcometrue.domain.city.entity.City;
 import com.haejwo.tripcometrue.domain.place.entity.Place;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
 
@@ -11,6 +12,8 @@ public interface PlaceCustomRepository {
 
     Page<Place> findPlaceWithFilter(Pageable pageable,
                                     Integer storedCount);
+
+    Slice<Place> findPlacesByCityId(Long cityId, Pageable pageable);
 
     List<Place> findPlacesByCityAndOrderByStoredCountLimitSize(City city, int size);
 }
