8dad3be688ed47df6d0c1e72468c0f5549726d58
jo0oy <rkfkrh07@naver.com>
2024-01-16 16:25:05 +0900

    [rename] 'HotPlaceResponseDto' to 'CityPlaceResponseDto' 파일명 변경
    related to: #66

--- src/main/java/com/haejwo/tripcometrue/domain/city/dto/response/CityPlaceResponseDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/city/dto/response/CityPlaceResponseDto.java
@@ -1,28 +1,27 @@
 package com.haejwo.tripcometrue.domain.city.dto.response;
 
 import com.haejwo.tripcometrue.domain.place.entity.Place;
-import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordScheduleImage;
 import lombok.Builder;
 
-import java.util.Objects;
-
-public record HotPlaceResponseDto(
+public record CityPlaceResponseDto(
     Long placeId,
     String placeName,
     Integer storedCount,
+    Integer commentTotal,
     String imageUrl
 ) {
 
     @Builder
-    public HotPlaceResponseDto {
+    public CityPlaceResponseDto {
     }
 
-    public static HotPlaceResponseDto fromEntity(Place entity, TripRecordScheduleImage image) {
-        return HotPlaceResponseDto.builder()
+    public static CityPlaceResponseDto fromEntity(Place entity, String imageUrl) {
+        return CityPlaceResponseDto.builder()
             .placeId(entity.getId())
             .placeName(entity.getName())
             .storedCount(entity.getStoredCount())
-            .imageUrl(Objects.isNull(image) ? null : image.getImageUrl())
+            .commentTotal(0) // TODO: 추후 댓글 수 로직 작성
+            .imageUrl(imageUrl)
             .build();
     }
 }
