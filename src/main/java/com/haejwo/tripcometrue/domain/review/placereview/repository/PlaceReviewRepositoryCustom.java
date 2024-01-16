4d20d07cdf031f11f37d5df1a889119e333087d1
kimdongmin <meena2003@naver.com>
2024-01-17 00:52:25 +0900

    [feat] 여행지 리뷰(PlaceReview) 리스트 조회 기능 구현.

--- src/main/java/com/haejwo/tripcometrue/domain/review/placereview/repository/PlaceReviewRepositoryCustom.java
+++ src/main/java/com/haejwo/tripcometrue/domain/review/placereview/repository/PlaceReviewRepositoryCustom.java
@@ -1,11 +1,11 @@
 package com.haejwo.tripcometrue.domain.review.placereview.repository;
 
+import com.haejwo.tripcometrue.domain.place.entity.Place;
 import com.haejwo.tripcometrue.domain.review.placereview.entity.PlaceReview;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface PlaceReviewRepositoryCustom {
 
-    Page<PlaceReview> findByPlaceId(@Param("placeId") Long placeId, Pageable pageable);
+    Page<PlaceReview> findByPlace(Place place, boolean onlyImage, Pageable pageable);
 }
