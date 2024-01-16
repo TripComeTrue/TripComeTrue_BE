247975d8aadcc7308759d065443aa9e666d7af45
jo0oy <rkfkrh07@naver.com>
2024-01-16 16:26:41 +0900

    [move] ExchangeRateUpdateScheduler import 변경
    related to: #66

--- src/main/java/com/haejwo/tripcometrue/domain/city/service/ExchangeRateUpdateScheduler.java
+++ src/main/java/com/haejwo/tripcometrue/domain/city/service/ExchangeRateUpdateScheduler.java
@@ -1,6 +1,6 @@
 package com.haejwo.tripcometrue.domain.city.service;
 
-import com.haejwo.tripcometrue.domain.city.dto.ExchangeRateApiDto;
+import com.haejwo.tripcometrue.domain.city.dto.api.ExchangeRateApiDto;
 import com.haejwo.tripcometrue.domain.city.entity.CurrencyUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
