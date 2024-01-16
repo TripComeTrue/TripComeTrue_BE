5edb95e03079eab286dc934168a8b5d93b733d33
jo0oy <rkfkrh07@naver.com>
2024-01-16 16:27:42 +0900

    [feat] 도시 상세 피드 조회 관련 응답 DTO 추가
    related to: #66

---
+++ src/main/java/com/haejwo/tripcometrue/domain/city/dto/response/CityImageContentResponseDto.java
@@ -0,0 +1,36 @@
+package com.haejwo.tripcometrue.domain.city.dto.response;
+
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecord;
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordSchedule;
+import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecordScheduleImage;
+import lombok.Builder;
+
+public record CityImageContentResponseDto(
+    Long id,
+    Long tripRecordId,
+    Integer storedCount,
+    String imageUrl
+) {
+    @Builder
+    public CityImageContentResponseDto {
+    }
+
+    public static CityImageContentResponseDto fromEntity(TripRecordSchedule entity, TripRecordScheduleImage image) {
+        return CityImageContentResponseDto.builder()
+            .id(image.getId())
+            .tripRecordId(entity.getTripRecord().getId())
+            .storedCount(entity.getTripRecord().getStoredCount())
+            .imageUrl(image.getImageUrl())
+            .build();
+    }
+
+    public static CityImageContentResponseDto fromEntity(TripRecordScheduleImage entity) {
+        TripRecord tripRecord = entity.getTripRecordSchedule().getTripRecord();
+        return CityImageContentResponseDto.builder()
+            .id(entity.getId())
+            .tripRecordId(tripRecord.getId())
+            .storedCount(tripRecord.getStoredCount())
+            .imageUrl(entity.getImageUrl())
+            .build();
+    }
+}
