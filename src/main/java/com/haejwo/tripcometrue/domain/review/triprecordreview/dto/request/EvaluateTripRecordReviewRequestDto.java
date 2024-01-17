165ce77fb2d08ebbde552e54d05ec781c0b27418
kimdongmin <meena2003@naver.com>
2024-01-17 13:35:06 +0900

    [feat] 여행 후기 리뷰(TripRecordReview) 별점 등록시 검증 기능 구현.
    0.5 단위로 입력되는지 검증 & 별점 범위(0.5 ~ 5.0) 검증 & null 검증.

--- src/main/java/com/haejwo/tripcometrue/domain/review/triprecordreview/dto/request/EvaluateTripRecordReviewRequestDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/review/triprecordreview/dto/request/EvaluateTripRecordReviewRequestDto.java
@@ -2,30 +2,29 @@ package com.haejwo.tripcometrue.domain.review.triprecordreview.dto.request;
 
 import com.haejwo.tripcometrue.domain.member.entity.Member;
 import com.haejwo.tripcometrue.domain.review.triprecordreview.entity.TripRecordReview;
+import com.haejwo.tripcometrue.domain.review.triprecordreview.validation.HalfUnit;
 import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecord;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 
 public record EvaluateTripRecordReviewRequestDto(
-        // TODO: 1/9/24
-        //  평점이 0.5 단위인지 확인하는 로직 추가.
-        //  0 ~ 5점 범위 검증. 아니면 예외 발생
-        @NotNull(message = "평점은 필수 항목입니다.")
-        Short rating
+
+        @NotNull(message = "별점은 필수 항목입니다.")
+        @DecimalMin(value = "0.5", message = "0.5점 아래로 평가할 순 없습니다.")
+        @Max(value = 5, message = "5점을 초과할 순 없습니다.")
+        @HalfUnit(message = "별점은 0.5 단위로 입력해야 합니다.")
+        Float ratingScore
 
 ) {
-        @Builder
-        public EvaluateTripRecordReviewRequestDto(Short rating) {
-                this.rating = rating;
-        }
 
-        public TripRecordReview toEntity(Member member, TripRecord tripRecord) {
-                return TripRecordReview.builder()
-                        .member(member)
-                        .tripRecord(tripRecord)
-                        .rating(this.rating)
-                        .build();
-        }
+    public TripRecordReview toEntity(Member member, TripRecord tripRecord) {
+        return TripRecordReview.builder()
+                .member(member)
+                .tripRecord(tripRecord)
+                .ratingScore(this.ratingScore)
+                .build();
+    }
 }
 
 
