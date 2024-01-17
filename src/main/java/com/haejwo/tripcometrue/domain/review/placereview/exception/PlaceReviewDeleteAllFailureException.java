ac859c3a34f30604ad55a0dc7e5de51af3facacc
kimdongmin <meena2003@naver.com>
2024-01-18 00:11:25 +0900

    [refactor] PlaceReview 예외들의 ErrorCode 메시지에서 EXCEPTION 제거

--- src/main/java/com/haejwo/tripcometrue/domain/review/placereview/exception/PlaceReviewDeleteAllFailureException.java
+++ src/main/java/com/haejwo/tripcometrue/domain/review/placereview/exception/PlaceReviewDeleteAllFailureException.java
@@ -5,7 +5,7 @@ import com.haejwo.tripcometrue.global.exception.ErrorCode;
 
 public class PlaceReviewDeleteAllFailureException extends ApplicationException {
 
-    private static final ErrorCode ERROR_CODE = ErrorCode.PLACE_REVIEW_DELETE_ALL_FAILURE_EXCEPTION;
+    private static final ErrorCode ERROR_CODE = ErrorCode.PLACE_REVIEW_DELETE_ALL_FAILURE;
 
     public PlaceReviewDeleteAllFailureException() {
         super(ERROR_CODE);
