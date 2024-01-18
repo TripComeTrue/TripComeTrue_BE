ad7acc15ce2e247fd64b005d2bdb17d940b296ab
INKWON <dlsrnjs066@gmail.com>
2024-01-19 03:10:28 +0900

    [fix] MyPage 관련 requestMatcher 수정

--- src/main/java/com/haejwo/tripcometrue/global/springsecurity/SpringSecurityConfig.java
+++ src/main/java/com/haejwo/tripcometrue/global/springsecurity/SpringSecurityConfig.java
@@ -72,13 +72,17 @@ public class SpringSecurityConfig {
             .requestMatchers(new AntPathRequestMatcher("/v1/member/check-duplicated-email")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/v1/member/oauth2/info/**")).permitAll()
 
+            .requestMatchers(new AntPathRequestMatcher("/v1/member/check-password")).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/v1/member/change-password")).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/v1/member/profile-image")).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/v1/member/introduction")).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/v1/member/nickname")).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/v1/member/nickname")).permitAll()
+            .requestMatchers(new AntPathRequestMatcher("/v1/view-history")).permitAll()
+
             .requestMatchers(new AntPathRequestMatcher("/v1/cities/**")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/v1/places/**")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/v1/images/**")).permitAll()
-
-            .requestMatchers(new AntPathRequestMatcher("/v1/likes/**")).permitAll()
-
-            .requestMatchers(new AntPathRequestMatcher("/v1/store/**")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/v1/trip-places/**")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/v1/trip-records/**")).permitAll()
             .requestMatchers(new AntPathRequestMatcher("/v1/trip-records-schedules/**")).permitAll()
