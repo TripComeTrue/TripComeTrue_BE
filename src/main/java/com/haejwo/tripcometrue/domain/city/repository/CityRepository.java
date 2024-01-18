e25698bcac1c224023db80a735c0eadb9436b05f
jo0oy <rkfkrh07@naver.com>
2024-01-18 10:45:10 +0900

    [feat] 국내/해외 인기 도시 리스트 조회 쿼리 구현
    related to: #80

--- src/main/java/com/haejwo/tripcometrue/domain/city/repository/CityRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/city/repository/CityRepository.java
@@ -2,10 +2,12 @@ package com.haejwo.tripcometrue.domain.city.repository;
 
 import com.haejwo.tripcometrue.domain.city.entity.City;
 import com.haejwo.tripcometrue.global.enums.Country;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CityRepository extends JpaRepository<City, Long> {
+import java.util.Optional;
+
+public interface CityRepository
+    extends JpaRepository<City, Long>, CityRepositoryCustom {
 
     Optional<City> findByNameAndCountry(String name, Country country);
 }
