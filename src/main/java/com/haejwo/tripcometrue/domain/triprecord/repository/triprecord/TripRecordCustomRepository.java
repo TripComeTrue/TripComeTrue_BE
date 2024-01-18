69a3ffb03ca2ae0d63d342669ef1efc06f9d20d0
junmo95 <junmop950@gmail.com>
2024-01-18 18:26:09 +0900

    [feat] 여행후기 목록 응답 API 구조 및 응답 내용 변경

--- src/main/java/com/haejwo/tripcometrue/domain/triprecord/repository/triprecord/TripRecordCustomRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/repository/triprecord/TripRecordCustomRepository.java
@@ -1,13 +1,14 @@
 package com.haejwo.tripcometrue.domain.triprecord.repository.triprecord;
 
-import com.haejwo.tripcometrue.domain.triprecord.dto.response.ModelAttribute.TripRecordListRequestAttribute;
+import com.haejwo.tripcometrue.domain.triprecord.dto.request.ModelAttribute.TripRecordListRequestAttribute;
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.TripRecordListResponseDto;
 import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecord;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 
 public interface TripRecordCustomRepository {
 
-    List<TripRecord> findTripRecordWithFilter(Pageable pageable, TripRecordListRequestAttribute request);
+    List<TripRecordListResponseDto> finTripRecordWithFilter(Pageable pageable, TripRecordListRequestAttribute request);
 
     List<TripRecord> findTopTripRecordListDomestic(int size);
 
