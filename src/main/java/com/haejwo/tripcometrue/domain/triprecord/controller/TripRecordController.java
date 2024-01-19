0d1891dbaeb9abc8581a10bde0857b67b5ec4ad0
junmo95 <junmop950@gmail.com>
2024-01-19 13:10:32 +0900

    [feat] 여행후기 메인페이지 쇼츠 기능 Controller 추가

--- src/main/java/com/haejwo/tripcometrue/domain/triprecord/controller/TripRecordController.java
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/controller/TripRecordController.java
@@ -4,6 +4,8 @@ import com.haejwo.tripcometrue.domain.triprecord.dto.request.ModelAttribute.Trip
 import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord.TopTripRecordResponseDto;
 import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord.TripRecordDetailResponseDto;
 import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord.TripRecordListResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord_schedule_media.TripRecordHotShortsListResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord_schedule_media.TripRecordScheduleVideoDetailDto;
 import com.haejwo.tripcometrue.domain.triprecord.service.TripRecordService;
 import com.haejwo.tripcometrue.global.springsecurity.PrincipalDetails;
 import com.haejwo.tripcometrue.global.util.ResponseDTO;
@@ -90,4 +92,32 @@ public class TripRecordController {
             .body(responseBody);
 
     }
+
+    @GetMapping("hot-shorts-list")
+    public ResponseEntity<ResponseDTO<List<TripRecordHotShortsListResponseDto>>> tripRecordHotShortsList(
+        Pageable pageable
+    ) {
+        List<TripRecordHotShortsListResponseDto> responseDtos = tripRecordService.findTripRecordHotShortsList(pageable);
+
+        ResponseDTO<List<TripRecordHotShortsListResponseDto>> responseBody = ResponseDTO.okWithData(responseDtos);
+
+        return ResponseEntity
+            .status(responseBody.getCode())
+            .body(responseBody);
+    }
+
+    @GetMapping("shorts/{videoId}")
+    public ResponseEntity<ResponseDTO<TripRecordScheduleVideoDetailDto>> tripRecordShortsDetail(
+        @PathVariable Long videoId
+    ) {
+
+        TripRecordScheduleVideoDetailDto responseDto = tripRecordService.findTripRecordShortsDetail(videoId);
+
+        ResponseDTO<TripRecordScheduleVideoDetailDto> responseBody = ResponseDTO.okWithData(responseDto);
+
+        return ResponseEntity
+            .status(responseBody.getCode())
+            .body(responseBody);
+    }
+
 }
