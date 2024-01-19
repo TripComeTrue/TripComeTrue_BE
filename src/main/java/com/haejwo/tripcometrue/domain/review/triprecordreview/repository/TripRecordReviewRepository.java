8ecadd0081d900f976ef4d2bdd0f5a79ba480e7c
kimdongmin <meena2003@naver.com>
2024-01-19 11:33:34 +0900

    [feat] 여행 후기 리뷰(TripRecordReview) 리스트 조회시, 본문(content)가 없는 리뷰는 조회 대상에서 제외 기능 구현

--- src/main/java/com/haejwo/tripcometrue/domain/review/triprecordreview/repository/TripRecordReviewRepository.java
+++ src/main/java/com/haejwo/tripcometrue/domain/review/triprecordreview/repository/TripRecordReviewRepository.java
@@ -6,12 +6,15 @@ import com.haejwo.tripcometrue.domain.triprecord.entity.TripRecord;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TripRecordReviewRepository extends JpaRepository<TripRecordReview, Long> {
 
-  Page<TripRecordReview> findByMember(Member member, Pageable pageable);
+  @Query("select trr from TripRecordReview trr join fetch trr.member m where trr.member = :member and trr.content is not null")
+  Page<TripRecordReview> findByMember(@Param("member") Member member, Pageable pageable);
 
   boolean existsByMemberAndTripRecord(Member member, TripRecord tripRecord);
 }
