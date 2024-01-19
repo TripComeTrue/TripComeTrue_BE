5d3b2b04ee64cd4abcf8884c6533f8c06a4aaead
kimdongmin <meena2003@naver.com>
2024-01-19 19:45:17 +0900

    [feat] 여행 후기 리뷰(TripRecordReview) 삭제 기능 구현

--- src/main/java/com/haejwo/tripcometrue/domain/review/triprecordreview/controller/TripRecordReviewControllerAdvice.java
+++ src/main/java/com/haejwo/tripcometrue/domain/review/triprecordreview/controller/TripRecordReviewControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.haejwo.tripcometrue.domain.review.triprecordreview.controller;
 
 import com.haejwo.tripcometrue.domain.review.triprecordreview.exception.TripRecordReviewAlreadyExistsException;
+import com.haejwo.tripcometrue.domain.review.triprecordreview.exception.TripRecordReviewDeleteAllFailureException;
 import com.haejwo.tripcometrue.domain.review.triprecordreview.exception.TripRecordReviewNotFoundException;
 import com.haejwo.tripcometrue.global.util.ResponseDTO;
 import org.springframework.http.HttpStatus;
@@ -22,6 +23,15 @@ public class TripRecordReviewControllerAdvice {
 
     @ExceptionHandler(TripRecordReviewNotFoundException.class)
     public ResponseEntity<ResponseDTO<Void>> handleTripRecordReviewNotFoundException(TripRecordReviewNotFoundException e) {
+        HttpStatus status = e.getErrorCode().getHttpStatus();
+
+        return ResponseEntity
+                .status(status)
+                .body(ResponseDTO.errorWithMessage(status, e.getMessage()));
+    }
+
+    @ExceptionHandler(TripRecordReviewDeleteAllFailureException.class)
+    public ResponseEntity<ResponseDTO<Void>> handleTripRecordReviewDeleteAllFailureException(TripRecordReviewDeleteAllFailureException e) {
         HttpStatus status = e.getErrorCode().getHttpStatus();
 
         return ResponseEntity
