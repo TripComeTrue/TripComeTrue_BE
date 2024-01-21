406b87c70de5b585001bfe6385a67646cf2fbc1e
jo0oy <rkfkrh07@naver.com>
2024-01-22 04:31:24 +0900

    [remove] TripRecordScheduleVideoItemResponseDto 삭제
    related to: #89

--- src/main/java/com/haejwo/tripcometrue/domain/triprecord/dto/response/triprecord_schedule_media/TripRecordScheduleVideoItemResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord_schedule_media;
-
-public record TripRecordScheduleVideoItemResponseDto(
-    Long tripRecordId,
-    String tripRecordTitle,
-    Integer storedCount,
-    String thumbnail
-) {
-}
