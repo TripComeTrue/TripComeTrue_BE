64ae7fa5945a9cae8c08331969aeb9ea80c7ddd1
jo0oy <rkfkrh07@naver.com>
2024-01-22 04:35:07 +0900

    [feat] 크리에이트 검색 API 구현
    related to: #89

---
+++ src/main/java/com/haejwo/tripcometrue/domain/member/controller/MemberSearchController.java
@@ -0,0 +1,51 @@
+package com.haejwo.tripcometrue.domain.member.controller;
+
+import com.haejwo.tripcometrue.domain.member.dto.response.MemberDetailListItemResponseDto;
+import com.haejwo.tripcometrue.domain.member.dto.response.MemberSearchResultWithContentResponseDto;
+import com.haejwo.tripcometrue.domain.member.facade.MemberSearchFacade;
+import com.haejwo.tripcometrue.global.util.ResponseDTO;
+import com.haejwo.tripcometrue.global.util.SliceResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/v1/members")
+@RestController
+public class MemberSearchController {
+
+    private final MemberSearchFacade memberSearchFacade;
+
+    @GetMapping("/list")
+    public ResponseEntity<ResponseDTO<MemberSearchResultWithContentResponseDto>> searchByNicknameResultWithContent(
+        @RequestParam("query") String query
+    ) {
+        return ResponseEntity
+            .ok()
+            .body(
+                ResponseDTO.okWithData(
+                    memberSearchFacade.searchByNicknameResultWithContent(query)
+                )
+            );
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseDTO<SliceResponseDto<MemberDetailListItemResponseDto>>> searchByNicknamePagination(
+        @RequestParam("query") String query,
+        @PageableDefault(sort = "rating", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        return ResponseEntity
+            .ok()
+            .body(
+                ResponseDTO.okWithData(
+                    memberSearchFacade.searchByNicknamePagination(query, pageable)
+                )
+            );
+    }
+}
