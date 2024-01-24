cb3a468be87f6b6ccc14c3eb2b9ac8bf5588bc48
jo0oy <rkfkrh07@naver.com>
2024-01-24 10:47:28 +0900

    [test] 해시태그 여행후기 검색, 여행 경비 범위 여행후기 검색 서비스 로직 테스트 코드 작성 및 확인
    related to: #98

--- src/test/java/com/haejwo/tripcometrue/domain/triprecord/service/TripRecordServiceTest.java
+++ src/test/java/com/haejwo/tripcometrue/domain/triprecord/service/TripRecordServiceTest.java
@@ -4,6 +4,7 @@ import com.haejwo.tripcometrue.config.AbstractContainersSupport;
 import com.haejwo.tripcometrue.config.DatabaseCleanUpAfterEach;
 import com.haejwo.tripcometrue.domain.triprecord.dto.request.ModelAttribute.TripRecordSearchParamAttribute;
 import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord.TripRecordListItemResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.entity.type.ExpenseRangeType;
 import com.haejwo.tripcometrue.global.util.SliceResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -89,5 +90,41 @@ class TripRecordServiceTest extends AbstractContainersSupport {
 
         // then
         assertThat(result).hasSize(3);
+    }
+
+    @Test
+    void listTripRecordsByHashtag() {
+        // given
+        String hashtag = "자유여행";
+        PageRequest pageRequest = PageRequest.of(0, 2,
+            Sort.by(Sort.Order.desc("averageRating"), Sort.Order.desc("storeCount")));
+
+        // when
+        SliceResponseDto<TripRecordListItemResponseDto> result = tripRecordService.listTripRecordsByHashtag(hashtag, pageRequest);
+
+        // then
+        assertThat(result.content()).hasSize(2);
+        assertThat(result.currentPageNum()).isEqualTo(0);
+        assertThat(result.pageSize()).isEqualTo(2);
+        assertThat(result.first()).isTrue();
+        assertThat(result.last()).isFalse();
+    }
+
+    @Test
+    void listTripRecordsByExpenseRangeType() {
+        // given
+        ExpenseRangeType expenseRangeType = ExpenseRangeType.BELOW_200;
+        PageRequest pageRequest = PageRequest.of(0, 2,
+            Sort.by(Sort.Order.desc("averageRating"), Sort.Order.desc("storeCount")));
+
+        // when
+        SliceResponseDto<TripRecordListItemResponseDto> result = tripRecordService.listTripRecordsByExpenseRangeType(expenseRangeType, pageRequest);
+
+        // then
+        assertThat(result.content()).hasSize(1);
+        assertThat(result.currentPageNum()).isEqualTo(0);
+        assertThat(result.pageSize()).isEqualTo(2);
+        assertThat(result.first()).isTrue();
+        assertThat(result.last()).isTrue();
     }
 }
