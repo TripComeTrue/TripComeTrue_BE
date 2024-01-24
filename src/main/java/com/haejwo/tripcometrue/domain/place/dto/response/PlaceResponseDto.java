91702ca1254869eedec8bfe982b2894b06c5a4cc
junmo95 <junmop950@gmail.com>
2024-01-24 11:28:44 +0900

    [feat] 여행지 세부정보 응답 DTO 좌표, 전화번호 추가

--- src/main/java/com/haejwo/tripcometrue/domain/place/dto/response/PlaceResponseDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/place/dto/response/PlaceResponseDto.java
@@ -1,6 +1,7 @@
 package com.haejwo.tripcometrue.domain.place.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import com.haejwo.tripcometrue.domain.place.entity.Place;
 import java.time.LocalTime;
 import lombok.Builder;
@@ -9,35 +10,36 @@ public record PlaceResponseDto(
     String name,
     String address,
     String description,
+    String phoneNumber,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm") LocalTime weekdayOpenTime,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm") LocalTime weekdayCloseTime,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm") LocalTime weekendOpenTime,
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm") LocalTime weekendCloseTime,
+    Double latitude,
+    Double longitude,
     Integer storedCount,
     Long cityId
 ) {
 
     @Builder
-    public PlaceResponseDto(
-        Long id,
-        String name,
-        String address,
-        String description,
-        LocalTime weekdayOpenTime,
-        LocalTime weekdayCloseTime,
-        LocalTime weekendOpenTime,
-        LocalTime weekendCloseTime,
-        Integer storedCount,
-        Long cityId
-    ) {
+    public PlaceResponseDto(Long id, String name, String address, String description,
+        String phoneNumber,
+        @JsonFormat(shape = Shape.STRING, pattern = "HH:mm") LocalTime weekdayOpenTime,
+        @JsonFormat(shape = Shape.STRING, pattern = "HH:mm") LocalTime weekdayCloseTime,
+        @JsonFormat(shape = Shape.STRING, pattern = "HH:mm") LocalTime weekendOpenTime,
+        @JsonFormat(shape = Shape.STRING, pattern = "HH:mm") LocalTime weekendCloseTime,
+        Double latitude, Double longitude, Integer storedCount, Long cityId) {
         this.id = id;
         this.name = name;
         this.address = address;
         this.description = description;
+        this.phoneNumber = phoneNumber;
         this.weekdayOpenTime = weekdayOpenTime;
         this.weekdayCloseTime = weekdayCloseTime;
         this.weekendOpenTime = weekendOpenTime;
         this.weekendCloseTime = weekendCloseTime;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.storedCount = storedCount;
         this.cityId = cityId;
     }
@@ -48,10 +50,13 @@ public record PlaceResponseDto(
             .name(entity.getName())
             .address(entity.getAddress())
             .description(entity.getDescription())
+            .phoneNumber(entity.getPhoneNumber())
             .weekdayOpenTime(entity.getWeekdayOpenTime())
             .weekdayCloseTime(entity.getWeekdayCloseTime())
             .weekendOpenTime(entity.getWeekendOpenTime())
             .weekendCloseTime(entity.getWeekendCloseTime())
+            .latitude(entity.getLatitude())
+            .longitude(entity.getLongitude())
             .storedCount(entity.getStoredCount())
             .cityId(entity.getCity().getId())
             .build();
