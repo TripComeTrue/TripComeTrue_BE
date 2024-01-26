19b4e4a460878d4215e473d05e066f97156c6838
INKWON <dlsrnjs066@gmail.com>
2024-01-27 03:04:10 +0900

    [refactor] store Response Dto 불필요 필드 정리

--- src/main/java/com/haejwo/tripcometrue/domain/store/dto/response/CityStoreResponseDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/store/dto/response/CityStoreResponseDto.java
@@ -6,34 +6,14 @@ import com.haejwo.tripcometrue.global.enums.Country;
 
 public record CityStoreResponseDto(
     Long id,
-    String name,
-    String language,
-    String timeDifference,
-    String voltage,
-    String visa,
-    CurrencyUnit currency,
-    String weatherRecommendation,
-    String weatherDescription,
-    Country country,
-    Integer storeCount,
-    String imageUrl
+    Integer storeCount
 ) {
 
   public static CityStoreResponseDto fromEntity(CityStore cityStore) {
     City city = cityStore.getCity();
     return new CityStoreResponseDto(
         city.getId(),
-        city.getName(),
-        city.getLanguage(),
-        city.getTimeDifference(),
-        city.getVoltage(),
-        city.getVisa(),
-        city.getCurrency(),
-        city.getWeatherRecommendation(),
-        city.getWeatherDescription(),
-        city.getCountry(),
-        city.getStoreCount(),
-        city.getImageUrl()
+        city.getStoreCount()
     );
   }
 }
--- src/main/java/com/haejwo/tripcometrue/domain/store/dto/response/PlaceStoreResponseDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/store/dto/response/PlaceStoreResponseDto.java
@@ -7,43 +7,16 @@ import java.time.LocalTime;
 
 public record PlaceStoreResponseDto(
     Long id,
-    String name,
-    String address,
-    String description,
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH-mm-ss")
-    LocalTime weekdayOpenTime,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH-mm-ss")
-    LocalTime weekdayCloseTime,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH-mm-ss")
-    LocalTime weekendOpenTime,
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH-mm-ss")
-    LocalTime weekendCloseTime,
     Integer storedCount,
-    Double latitude,
-    Double longtitude,
-    Integer commentCount,
-    Long cityId,
-    String imageUrl
+    Long cityId
 ) {
 
   public static PlaceStoreResponseDto fromEntity(PlaceStore placeStore, String imageUrl) {
     Place place = placeStore.getPlace();
     return new PlaceStoreResponseDto(
         place.getId(),
-        place.getName(),
-        place.getAddress(),
-        place.getDescription(),
-        place.getWeekdayOpenTime(),
-        place.getWeekdayCloseTime(),
-        place.getWeekendOpenTime(),
-        place.getWeekendCloseTime(),
         place.getStoredCount(),
-        place.getLatitude(),
-        place.getLongitude(),
-        place.getCommentCount(),
-        place.getCity().getId(),
-        imageUrl
+        place.getCity().getId()
     );
   }
 }
