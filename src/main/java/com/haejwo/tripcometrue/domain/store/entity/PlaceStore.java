d110be31fcf98429f5ada73b69dfe78bc857d4ec
jo0oy <rkfkrh07@naver.com>
2024-01-26 17:56:12 +0900

    [refactor] PlaceStore 엔티티 설정 리팩토링
    - @NoArgsConstructor access level 'PROTECTED'로 변경
    - @ManyToOne 'optional=false' 조건 추가
      - null 방지
    - @Builder 생성자 접근자 private으로 변경
    
    related to: #127

--- src/main/java/com/haejwo/tripcometrue/domain/store/entity/PlaceStore.java
+++ src/main/java/com/haejwo/tripcometrue/domain/store/entity/PlaceStore.java
@@ -1,23 +1,17 @@
 package com.haejwo.tripcometrue.domain.store.entity;
+
 import com.haejwo.tripcometrue.domain.member.entity.Member;
 import com.haejwo.tripcometrue.domain.place.entity.Place;
 import com.haejwo.tripcometrue.global.entity.BaseTimeEntity;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class PlaceStore extends BaseTimeEntity {
 
     @Id
@@ -25,16 +19,16 @@ public class PlaceStore extends BaseTimeEntity {
     @Column(name = "place_store_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "member_id")
     Member member;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "place_id")
     Place place;
 
     @Builder
-    public PlaceStore(Member member, Place place){
+    private PlaceStore(Member member, Place place){
         this.member = member;
         this.place = place;
     }
