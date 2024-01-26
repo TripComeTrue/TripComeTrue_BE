8b4ca035bc174fa86c3656eabb238693140c1133
junmo95 <junmop950@gmail.com>
2024-01-27 02:29:59 +0900

    [feat] 여행지 갤러리 Page 정보 추가

--- src/main/java/com/haejwo/tripcometrue/domain/triprecord/service/TripRecordScheduleService.java
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/service/TripRecordScheduleService.java
@@ -3,8 +3,8 @@ package com.haejwo.tripcometrue.domain.triprecord.service;
 import com.haejwo.tripcometrue.domain.triprecord.dto.request.ModelAttribute.TripRecordScheduleImageListRequestAttribute;
 import com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord_schedule_media.TripRecordScheduleImageListResponseDto;
 import com.haejwo.tripcometrue.domain.triprecord.repository.triprecord_schedule.TripRecordScheduleRepository;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +14,12 @@ public class TripRecordScheduleService {
 
     private final TripRecordScheduleRepository tripRecordScheduleRepository;
 
-    public List<TripRecordScheduleImageListResponseDto> findScheduleImages(
+    public Page<TripRecordScheduleImageListResponseDto> findScheduleImages(
         Pageable pageable,
         TripRecordScheduleImageListRequestAttribute requestParam
     ) {
 
-        List<TripRecordScheduleImageListResponseDto> responseDtos = tripRecordScheduleRepository.findScheduleImagesWithFilter(pageable, requestParam);
+        Page<TripRecordScheduleImageListResponseDto> responseDtos = tripRecordScheduleRepository.findScheduleImagesWithFilter(pageable, requestParam);
 
         return responseDtos;
 
