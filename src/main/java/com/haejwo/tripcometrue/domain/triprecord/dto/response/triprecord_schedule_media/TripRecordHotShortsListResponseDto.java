9c3f8b3fea2e7832d944e642d3e645afd9600cdf
junmo95 <junmop950@gmail.com>
2024-01-28 20:57:52 +0900

    [feat] 여행후기 쇼츠 리스트 API 응답 데이터 추가

--- src/main/java/com/haejwo/tripcometrue/domain/triprecord/dto/response/triprecord_schedule_media/TripRecordHotShortsListResponseDto.java
+++ src/main/java/com/haejwo/tripcometrue/domain/triprecord/dto/response/triprecord_schedule_media/TripRecordHotShortsListResponseDto.java
@@ -1,6 +1,6 @@
 package com.haejwo.tripcometrue.domain.triprecord.dto.response.triprecord_schedule_media;
 
-import com.haejwo.tripcometrue.domain.triprecord.dto.response.member.TripRecordMemberResponseDto;
+import com.haejwo.tripcometrue.domain.triprecord.dto.response.member.TripRecordVideoMemberResponseDto;
 import lombok.Builder;
 
 public record TripRecordHotShortsListResponseDto(
@@ -10,19 +10,21 @@ public record TripRecordHotShortsListResponseDto(
     Integer tripRecordStoreCount,
     Long videoId,
     String thumbnailUrl,
-    TripRecordMemberResponseDto member
+    String videoUrl,
+    TripRecordVideoMemberResponseDto member
 
 ) {
 
     @Builder
     public TripRecordHotShortsListResponseDto(Long tripRecordId, String tripRecordTitle,
-        Integer tripRecordStoreCount, Long videoId, String thumbnailUrl,
-        TripRecordMemberResponseDto member) {
+        Integer tripRecordStoreCount, Long videoId, String thumbnailUrl, String videoUrl,
+        TripRecordVideoMemberResponseDto member) {
         this.tripRecordId = tripRecordId;
         this.tripRecordTitle = tripRecordTitle;
         this.tripRecordStoreCount = tripRecordStoreCount;
         this.videoId = videoId;
         this.thumbnailUrl = thumbnailUrl;
+        this.videoUrl = videoUrl;
         this.member = member;
     }
 }
